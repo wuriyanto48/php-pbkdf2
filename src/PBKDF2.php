c9344ab92c188a8493af2e46016d36a08bbd083c
wuriyanto48 <wuriyanto48@yahoo.co.id>
2026-02-03 16:50:05 +0700

    initial commit

---
+++ src/PBKDF2.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Wuriyanto\PhpPbkdf2;
+
+class PBKDF2 {
+
+    private string $digest;
+    private int $salt_size;
+    private int $key_length;
+    private int $iterations;
+
+    private function __construct(string $digest, int $salt_size, int $key_length, int $iterations) {
+        $this->digest = $digest;
+        $this->salt_size = $salt_size;
+        $this->key_length = $key_length;
+        $this->iterations = $iterations;
+
+    }
+
+    public static function create(
+        string $digest = 'sha256', 
+        int $salt_size = 16, 
+        int $key_length = 32, 
+        int $iterations = 1000
+    ): PBKDF2 {
+        return new self($digest, $salt_size, $key_length, $iterations);
+    }
+
+    public function hash(string $password): string {
+        $salt = $this->generateSalt();
+        $hash = hash_pbkdf2(
+            $this->digest, 
+            $password, 
+            $salt, 
+            $this->iterations, 
+            $this->key_length, 
+            true
+        );
+        return $salt . '|' . base64_encode($hash);
+    }
+
+    public function verify(string $password, string $hash): bool {
+        $parts = explode('|', $hash);
+        if (count($parts) !== 2) {
+            return false;
+        }
+
+        [$saltB64, $hashB64] = $parts;
+
+        $calculated_hash = hash_pbkdf2(
+            $this->digest, 
+            $password, 
+            $saltB64, 
+            $this->iterations, 
+            $this->key_length, 
+            true
+        );
+
+        return hash_equals($calculated_hash, base64_decode($hashB64));
+    }
+
+    private function generateSalt(): string {
+        return base64_encode(random_bytes($this->salt_size));
+    }
+}
---
+++ tests/PBKDF2Test.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Wuriyanto\PhpPbkdf2\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Wuriyanto\PhpPbkdf2\PBKDF2;
+
+final class PBKDF2Test extends TestCase
+{
+
+    public function testHashAndVerify(): void
+    {
+        $pbkdf2 = PBKDF2::create('sha256', 16, 32, 1000);
+        $password = 'secure_password';
+
+        $hash = $pbkdf2->hash($password);
+        print($hash . PHP_EOL);
+        
+        $this->assertIsString($hash);
+
+        $isValid = $pbkdf2->verify($password, $hash);
+        $this->assertTrue($isValid);
+
+        $isInvalid = $pbkdf2->verify('wrong_password', $hash);
+        $this->assertFalse($isInvalid);
+    }
+
+    public function testVerifyFromHash1(): void
+    {
+        $pbkdf2 = PBKDF2::create('sha1', 8, 32, 15000);
+
+        $hash = 'lPeWBK4RMSg=|Z0IUQ+7lIm+EiQotZd3/53StMUIfToYZuhyzMAjMqe0=';
+        $password = '123456';
+
+        $isValid = $pbkdf2->verify($password, $hash);
+        $this->assertTrue($isValid);
+    }
+
+}
